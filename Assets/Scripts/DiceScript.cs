4e7dc4485d0d44bc2f6783dd7aac38eb4eeabfb0
CazNm <namaga132@gmail.com>
2020-07-21 01:48:50 +0900

    roll dice

---
+++ Assets/Scripts/DiceScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScript : MonoBehaviour {
+
+	static Rigidbody rb;
+	Vector3 currentPos;
+	public static Vector3 diceVelocity;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		float rdirX = Random.Range(-4, 4);
+		float rdirZ = Random.Range(-4, 4);
+		
+		transform.position = new Vector3(rdirX, 0.5f, rdirZ);
+		currentPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		diceVelocity = rb.velocity;
+	}
+
+	public void Reroll() {
+		diceVelocity = rb.velocity;
+		DiceNumberTextScript.diceNumber = 0;
+		float dirX = Random.Range(100, 500);
+		float dirY = Random.Range(100, 500);
+		float dirZ = Random.Range(100, 500);
+		transform.position = new Vector3(currentPos.x, 2, currentPos.z);
+		transform.rotation = Quaternion.identity;
+		rb.AddForce(transform.up * 500);
+		rb.AddTorque(dirX, dirY, dirZ);
+	}
+}
---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Roll : MonoBehaviour
+{
+
+    Button BT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BT = this.transform.GetComponent<Button>();
+        BT.onClick.AddListener(Rolldice);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Rolldice() {
+        GameObject dice1 = GameObject.Find("dice1");
+        GameObject dice2 = GameObject.Find("dice2");
+        //GameObject dice3 = GameObject.Find("dice3");
+        //GameObject dice4 = GameObject.Find("dice4");
+        //GameObject dice5 = GameObject.Find("dice5");
+
+        dice1.GetComponent<DiceScript>().Reroll();
+        dice2.GetComponent<DiceScript>().Reroll();
+        //dice3.GetComponent<DiceScript>().Reroll();
+        //dice4.GetComponent<DiceScript>().Reroll();
+        //dice5.GetComponent<DiceScript>().Reroll();
+
+    }
+}
