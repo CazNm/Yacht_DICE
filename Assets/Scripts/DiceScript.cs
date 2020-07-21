0e98d94984d3c0b445fea2d349ebddebeb451b7d
CazNm <namaga132@gmail.com>
2020-07-21 12:48:35 +0900

    roll 5 dice

--- Assets/Scripts/DiceScript.cs
+++ Assets/Scripts/DiceScript.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class DiceScript : MonoBehaviour {
 
-	static Rigidbody rb;
-	Vector3 currentPos;
+	Rigidbody rb;
+	Transform transform;
+
+	
+	private float rdirX;
+	private float rdirZ;
+
+	public Vector3 currentPos;
 	public static Vector3 diceVelocity;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
-		float rdirX = Random.Range(-4, 4);
-		float rdirZ = Random.Range(-4, 4);
-		
-		transform.position = new Vector3(rdirX, 0.5f, rdirZ);
+		rb = this.GetComponent<Rigidbody> ();
+		transform = this.GetComponent<Transform>();
+		rdirX = Random.Range(-4, 4);
+		rdirZ = Random.Range(-4, 4);
+
+		float rotX = Random.Range(0, 360);
+		float rotY = Random.Range(0, 360);
+		float rotZ = Random.Range(0, 360);
+
+		transform.position = new Vector3(rdirX, 1f, rdirZ);
+		transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
 		currentPos = transform.position;
 		
 	}
@@ -22,6 +34,15 @@ public class DiceScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		diceVelocity = rb.velocity;
+		currentPos = transform.position;
+
+		if (currentPos.y < -0.5f)
+        {
+			Debug.Log("out of box");
+			transform.position = new Vector3(rdirX, 1f ,rdirZ);
+        }
+
+
 	}
 
 	public void Reroll() {
@@ -30,8 +51,8 @@ public class DiceScript : MonoBehaviour {
 		float dirX = Random.Range(100, 500);
 		float dirY = Random.Range(100, 500);
 		float dirZ = Random.Range(100, 500);
-		transform.position = new Vector3(currentPos.x, 2, currentPos.z);
-		transform.rotation = Quaternion.identity;
+		transform.position = new Vector3(currentPos.x, 5, currentPos.z);
+		//transform.rotation = Quaternion.identity;
 		rb.AddForce(transform.up * 500);
 		rb.AddTorque(dirX, dirY, dirZ);
 	}
