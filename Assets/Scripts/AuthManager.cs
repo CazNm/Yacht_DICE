02d27f92479efb08a8232f7b6032387ee0875663
CazNm <namaga132@gmail.com>
2020-07-25 02:52:18 +0900

    firebase

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,127 @@
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using Photon.Pun.Demo.Cockpit.Forms;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    public bool IsFirebaseReady { get; private set; }
+    public bool IsSignInOnProgress { get; private set; }
+    public bool IsSignUpOnProgress { get; private set; }
+
+    public InputField emailField;
+    public InputField passwordField;
+    public Button signInButton;
+    public Button singUPButton;
+
+    public static FirebaseApp firebaseApp;
+    public static FirebaseAuth firebaseAuth;
+
+    public static FirebaseUser User;
+
+    public void Start()
+    {
+        signInButton.interactable = false;
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            var result = task.Result;
+
+            if (result != DependencyStatus.Available)
+            {
+                Debug.LogError(result.ToString());
+                IsFirebaseReady = false;
+            }
+            else
+            {
+                IsFirebaseReady = true;
+
+                firebaseApp = FirebaseApp.DefaultInstance;
+                firebaseAuth = FirebaseAuth.DefaultInstance;
+            }
+
+            signInButton.interactable = IsFirebaseReady;
+        });
+    }
+
+    public void SignIn()
+    {
+        if (emailField.text.Length == 0 || passwordField.text.Length == 0) return;
+        if (!IsFirebaseReady || IsSignInOnProgress || IsSignUpOnProgress ||User != null ) return;
+
+        IsSignInOnProgress = true;
+        signInButton.interactable = false;
+
+        firebaseAuth.SignInWithEmailAndPasswordAsync(emailField.text, passwordField.text).ContinueWithOnMainThread(
+            task =>
+            {
+                Debug.Log($"Sign in status : {task.Status}");
+
+                IsSignInOnProgress = false;
+                signInButton.interactable = true;
+
+                if (task.IsFaulted)
+                {
+                    Debug.LogError(task.Exception);
+                }
+                else if (task.IsCanceled)
+                {
+                    Debug.LogError("It's canceled");
+                }
+                else
+                {
+                    User = task.Result;
+                    SceneManager.LoadScene("Lobby");
+                }
+            });
+    }
+
+    public void SingUp() {
+        if (emailField.text.Length == 0 || passwordField.text.Length == 0) return;
+        if (!IsFirebaseReady || IsSignInOnProgress || IsSignUpOnProgress || User != null) return;
+
+        firebaseAuth.CreateUserWithEmailAndPasswordAsync(emailField.text, passwordField.text).ContinueWithOnMainThread(
+            task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("Create Cancled");
+                }
+                else if (task.IsFaulted)
+                {
+                    Debug.LogError(task.Exception);
+                }
+                else {
+                    GameObject.Find("Canvas").transform.Find("SignUpComp").gameObject.SetActive(true);
+                }
+
+            });
+    
+    }
+
+    public void ActiveLoginUI() {
+        GameObject.Find("Canvas").transform.Find("LS").gameObject.SetActive(true);
+    }
+
+    public void UnactiveLoginUI() { 
+        GameObject.Find("Canvas").transform.Find("LS").gameObject.SetActive(false); 
+    }
+
+
+    //나중에 구글플레이 연동시 사용할 코드
+
+    /* public void googleSignIn() {
+         if (!IsFirebaseReady || IsSignInOnProgress || User != null) return;
+
+         IsSignInOnProgress = true;
+         signInButton.interactable = false;
+
+         Firebase.Auth.Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
+         { }
+     }
+
+     */
+}
---
+++ Assets/Scripts/AutoUnActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoUnActive : MonoBehaviour
+{
+
+    public float timer;
+    public float wait_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        wait_time = 0.7f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > wait_time) {
+            gameObject.SetActive(false);
+        }
+    }
+}
